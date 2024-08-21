b1d156ed651e8a96e91bdbab44909d3751475a92
XuanBac2004 <thanhnha041225@gmail.com>
2024-08-21 22:58:58 +0700

    test frontend

---
+++ Collapsible content 2/index.js
@@ -0,0 +1,14 @@
+document.getElementById('dropdown').addEventListener('click', function() {
+    var content = document.getElementById('dropdown-content');
+    var icon = document.getElementById('dropdown-icon');
+
+    content.classList.toggle('show');
+    
+    if (icon.classList.contains('rotate')) {
+        icon.classList.remove('rotate');
+        icon.classList.add('rotate-back');
+    } else {
+        icon.classList.remove('rotate-back');
+        icon.classList.add('rotate');
+    }
+});
